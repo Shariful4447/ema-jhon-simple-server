ff3a6ccc3d098593d3491f54e37d94ce9f545b79
Shariful4447 <shariful.islam4447@gmail.com>
2023-05-11 00:19:29 +0200

    server side started connection process with database

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
+
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World! ema jhon')
-})
 
-app.post('/', (req, res) => {
-    res.send('post started here');
-})
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASS}@cluster0.w2aoa6h.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port)
