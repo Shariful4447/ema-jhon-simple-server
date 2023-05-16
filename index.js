bdd842ffc98594a0709d11f1e481ac6ff760f783
Shariful4447 <shariful.islam4447@gmail.com>
2023-05-16 21:20:50 +0200

    server side started for crud connection established

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 // middleware
 app.use(cors());
@@ -30,29 +30,8 @@ async function run() {
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
-    app.get('/products', async (req, res) => {
-      console.log(req.query);
-      const page = parseInt(req.query.page) || 0;
-      const limit = parseInt(req.query.limit) || 10;
-      const skip = page * limit;
 
-      const result = await productCollection.find().skip(skip).limit(limit).toArray();
-      res.send(result);
-    })
-
-    app.get('/totalProducts', async (req, res) => {
-      const result = await productCollection.estimatedDocumentCount();
-      res.send({ totalProducts: result })
-    })
-
-    app.post('/productsByIds', async(req, res) => {
-      const ids = req.body;
-      const objectIds = ids.map(id => new ObjectId(id));
-      const query = { _id: { $in: objectIds } }
-      console.log(ids);
-      const result = await productCollection.find(query).toArray();
-      res.send(result);
-    })
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
